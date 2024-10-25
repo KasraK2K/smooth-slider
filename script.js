14880f5766ef6550b4db8f799eae8e672204fca5
Kasra Karami <Kasra.Karami.KK@gmail.com>
2024-10-26 01:12:56 +0330

    complete project

---
+++ script.js
@@ -0,0 +1,47 @@
+const nextDom = document.getElementById("next");
+const prevDom = document.getElementById("prev");
+const carouselDom = document.querySelector(".carousel");
+const listItemDom = document.querySelector(".carousel .list");
+const thumbnailDom = document.querySelector(".carousel .thumbnail");
+
+nextDom.onclick = function () {
+  showSlider("next");
+};
+
+prevDom.onclick = function () {
+  showSlider("prev");
+};
+
+const timeRunning = 2000;
+const timeAutoNext = 7000;
+let runTimeOut;
+let runAutoRun = setTimeout(() => {
+  prevDom.click();
+}, timeAutoNext);
+
+function showSlider(type) {
+  const itemSlider = document.querySelectorAll(".carousel .list .item");
+  const itemThumbnail = document.querySelectorAll(".carousel .thumbnail .item");
+
+  if (type === "next") {
+    listItemDom.appendChild(itemSlider[0]);
+    thumbnailDom.appendChild(itemThumbnail[0]);
+    carouselDom.classList.add("next");
+  } else {
+    let positionLastItem = itemSlider.length - 1;
+    listItemDom.prepend(itemSlider[positionLastItem]);
+    thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+    carouselDom.classList.add("prev");
+  }
+
+  clearTimeout(runTimeOut);
+  runTimeOut = setTimeout(() => {
+    carouselDom.classList.remove("next");
+    carouselDom.classList.remove("prev");
+  }, timeRunning);
+
+  clearTimeout(runAutoRun);
+  runAutoRun = setTimeout(() => {
+    nextDom.click();
+  }, timeAutoNext);
+}
